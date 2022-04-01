71b0b2a30b2e51e3e22d220069f09bfa9ccd591b
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-04-01 15:07:55 +0800

    Update emacs config
    - also add dap-breakpoints to gitignore

--- init.el
+++ init.el
@@ -142,3 +142,5 @@
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
+(global-set-key (kbd "C-c ;") #'comment-line)
+(global-set-key (kbd "C-c C-;") #'comment-region)
